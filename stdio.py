54338dc63333305ea52968ecd494b954c3005772
lam95 <lamnguyen31195@gmail.com>
2017-07-21 15:44:12 +0700

    stdio

---
+++ stdio.py
@@ -0,0 +1,7 @@
+#-*- coding: utf-8 -*-
+print "lenh in ra man hinh: print"
+print "hello world"
+print "Viết tiếng Việt: #-*- coding: utf-8 -*-"
+print "nhap tu ban phim: raw_input()"
+var = raw_input("Nhap vao gia tri bien var:")
+print "Gia tri bien var:", var
